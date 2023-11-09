fed13905838e266ac0d9ddbf22fc304aca915628
firegodjr <firegodjr.es@gmail.com>
2023-11-08 23:10:07 -0600

    Add a bunch of styles and basic structure

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NavFrame />
+  <h1 class="text-3xl font-bol underline">HELLO TAILWIND!!!!</h1>
 </template>
+
+<script setup>
+import NavFrame from "./ui/nav_frame.vue";
+</script>
+
+<style>
+body { 
+  height: 100vh;
+  background: rgb(0,22,20);
+  background: linear-gradient(0deg, rgb(0, 14, 12) 0%, #27080f 100%);
+
+  text-shadow: 0 0 7px var(--clr-text-red);
+  color: var(--clr-text-red);
+}
+</style>
---
+++ ui/nav_frame.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="wrapper">
+        <div class="topnav">
+            <div>Inventories</div>
+            <div>Architectures</div>
+            <div>Account</div>
+        </div>
+        <div class="line red-box"></div>
+        <div class="content-frame">
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+.topnav {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    font-weight: bold;
+}
+
+.topnav > div {
+    text-align: center;
+    padding: 0.5em;
+    font-size: larger;
+}
+</style>
